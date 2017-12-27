e59f1604acf81ca4658c973ffeeda6e3320dcb93
Cristiano <31051360+CristianoCM@users.noreply.github.com>
2017-12-27 15:54:15 -0200

    Rename Problem58.rb to problem58.rb

--- problem58.rb
+++ problem58.rb
@@ -9,7 +9,7 @@ Starting with 1 and spiralling anticlockwise in the following way, a square spir
 42 21 22 23 24 25 26
 43 44 45 46 47 48 49
 
-It is interesting to note that the odd squares lie along the bottom right diagonal, but what is more interesting is that 8 out of the 13 numbers lying along both diagonals are prime; that is, a ratio of 8/13 ˜ 62%.
+It is interesting to note that the odd squares lie along the bottom right diagonal, but what is more interesting is that 8 out of the 13 numbers lying along both diagonals are prime; that is, a ratio of 8/13 Ëœ 62%.
 
 If one complete new layer is wrapped around the spiral above, a square spiral with side length 9 will be formed. If this process is continued, what is the side length of the square spiral for which the ratio of primes along both diagonals first falls below 10%?
 =end
@@ -18,8 +18,8 @@ require 'prime'
 
 class Problem58
   
-  # Proporção de crescimento: 1 8 16 24 36 ...
-  # Proporção de números pulados para chegar na diagonal: 0 1 3 5 7 ...
+  # ProporÃ§Ã£o de crescimento: 1 8 16 24 36 ...
+  # ProporÃ§Ã£o de nÃºmeros pulados para chegar na diagonal: 0 1 3 5 7 ...
   mul = 1
   nums = [1]
   num_now = 1
